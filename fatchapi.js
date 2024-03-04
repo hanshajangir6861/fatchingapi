445f782f7073f6dc57b1c74f2b79bfcd59c4dc12
unknown <hanshajangir061@gmail.com>
2024-03-03 19:07:12 -0800

    finish

--- fatchapi.js
+++ fatchapi.js
@@ -1,26 +1,24 @@
 
 
 const URL = 'https://cat-fact.herokuapp.com/facts'
-const factpara = document.querySelector('#fact')
+const factPara = document.querySelector('#fact')
 const btn = document.querySelector("#btn")
 
-const getdata = async () =>{
+const getdata = async () => {
     console.log("getting data .....")
-let response = await  fetch(URL)
-console.log(response)
-let data = await response.json()
-console.log(data[0].text)
-factpara.innerHTML = data[0].text
+    let response = await fetch(URL)
+    console.log(response)
+    let data = await response.json()
+    // console.log(data[0].text)
+    factPara.innerText = data[0].text
 }
-
-
-// function create() { 
-//             let div = document.createElement('div'); 
-//             div.textContent = "Geeksforgeeks"; 
-//             div.setAttribute('class', 'note'); 
-//             document.body.appendChild(div); 
+btn.addEventListener("click", getdata)
+// function create() {
+//             let div = document.createElement('div');
+//             div.textContent = "Geeksforgeeks";
+//             div.setAttribute('class', 'note');
+//             document.body.appendChild(div);
 //             console.log()
-//         } 
+//         }
 
 //         create()
-   
